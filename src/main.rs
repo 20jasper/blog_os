43551feb6992b599886334f052d1093c7ce38163
Jacob Asper <jacobasper191@gmail.com>
2023-04-10 23:47:29 -0400

    add entry point

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
@@ -7,4 +8,15 @@ fn panic(_info: &PanicInfo) -> ! {
 	loop {}
 }
 
-fn main() {}
+// the no_mangle attribute ensures the rust compiler does not change the name of
+// the _start function
+#[no_mangle]
+// this function is marked with "extern C" since it uses the C convention for
+// calling a function
+//
+// the "!" return type means it has a divergent return type, or it never
+// returns, since it will never be called, only invoked directly
+// "since there’s nothing left to do if a freestanding binary returns"
+pub extern "C" fn _start() -> ! {
+	panic!()
+}
