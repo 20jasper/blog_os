dabc5c47b68272e2fe4159d4f837232aa33715c0
Jacob Asper <jacobasper191@gmail.com>
2023-04-25 18:29:03 -0400

    add extract logic printing to buffer to a function

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use core::panic::PanicInfo;
 
 mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
 // the no_mangle attribute ensures the rust compiler does not change the name of
 // the _start function
 #[no_mangle]
@@ -17,20 +16,7 @@ static HELLO: &[u8] = b"Hello World!";
 // divergent return type, or it never only invoked directly "since there’s
 // nothing left to do if a freestanding binary returns"
 pub extern "C" fn _start() -> ! {
-	// cast vga_buffer as a raw pointer, which aren't automatically cleaned up,
-	// can be null, and aren't guarunteed to point to valid memory
-	let vga_buffer = 0xb8000 as *mut u8;
-
-	for (i, &byte) in HELLO.iter().enumerate() {
-		unsafe {
-			// the dereference operator (*) is needed to get the value from the memory
-			// address so we can change it to the bytes in our message
-
-			*vga_buffer.offset(i as isize * 2) = byte;
-			// write color byte
-			*vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-		}
-	}
+	vga_buffer::print_something();
 
 	#[allow(clippy::empty_loop)]
 	loop {}
