49f5dda52eca71d29f3ab6f4e59649e52943be27
Jacob Asper <jacobasper191@gmail.com>
2023-04-25 00:46:47 -0400

    add screen char and text buffer structs

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,11 +1,15 @@
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 // store each variant as a u8
+// Does this mean the enum can only have 255 discriminants or the fields' values
+// can only go up to 255???
+//
 // https://doc.rust-lang.org/nomicon/other-reprs.html#repru-repri
 // the effect is similar to the effect of repr(C) in that there is a defined
 // layout of the type. This makes it possible to pass the enum to C code, or
 // access the type's raw representation and directly manipulate its tag and
 // fields
+// https://doc.rust-lang.org/reference/type-layout.html#primitive-representation-of-enums-with-fields
 #[repr(u8)]
 pub enum Color {
 	Black = 0,
@@ -30,6 +34,7 @@ pub enum Color {
 // similar to the repr trait above—defines the layout of the struct as a u8 in
 // this case
 // https://doc.rust-lang.org/nomicon/other-reprs.html#reprtransparent
+// https://doc.rust-lang.org/reference/type-layout.html#the-transparent-representation
 // is it the same as using the repr(u8) trait though??
 #[repr(transparent)]
 // tuple struct
@@ -39,4 +44,21 @@ impl ColorCode {
 	fn new(foreground: Color, background: Color) -> ColorCode {
 		ColorCode((background as u8) << 4 | (foreground as u8))
 	}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+// orders the struct exactly like in C
+// https://doc.rust-lang.org/reference/type-layout.html#reprc-structs
+#[repr(C)]
+struct ScreenChar {
+	ascii_character: u8,
+	color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[repr(transparent)]
+struct Buffer {
+	chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
